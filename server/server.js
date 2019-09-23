11e71ab7d0258dd06048516043d5e7615a2a76a3
NicolaAstra <mcsnow78@gmail.com>
2019-09-23 22:02:03 +0700

    add brands model

--- server/server.js
+++ server/server.js
@@ -15,9 +15,34 @@ app.use(cookieParser());
 
 // Models
 const { User } = require("./models/user");
+const { Brand } = require("./models/brand");
 
-//Middlewares
+//Middleware
 const { auth } = require("./middleware/auth");
+const { admin } = require("./middleware/admin");
+
+// =========================
+//          BRAND
+// =========================
+
+app.post("/api/product/brand", auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
+
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      brand: doc
+    });
+  });
+});
+
+app.get("/api/product/brands", (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(brands);
+  });
+});
 
 // =========================
 //          USERS
