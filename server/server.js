2d2db1d420f594970eceab9f30cb68e8339110cc
NicolaAstra <mcsnow78@gmail.com>
2019-09-23 20:39:54 +0700

    add logout

--- server/server.js
+++ server/server.js
@@ -72,6 +72,15 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    });
+  });
+});
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
