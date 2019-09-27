5fbde680a4a324d580afe56d36b9dba9cf2cf059
NicolaAstra <mcsnow78@gmail.com>
2019-09-27 10:15:25 +0700

    add find product by id

--- server/server.js
+++ server/server.js
@@ -27,6 +27,26 @@ const { admin } = require("./middleware/admin");
 //          PRODUCTS
 // =========================
 
+app.get("/api/product/articles_by_id", (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
+
+  if (type === "array") {
+    let ids = req.query.id.split(",");
+    items = [];
+    items = ids.map(item => {
+      return mongoose.Types.ObjectId(item);
+    });
+  }
+
+  Product.find({ _id: { $in: items } })
+    .populate("brand")
+    .populate("wood")
+    .exec((err, docs) => {
+      return res.status(200).send(docs);
+    });
+});
+
 app.post("/api/product/article", auth, admin, (req, res) => {
   const product = new Product(req.body);
 
