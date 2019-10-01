9f90069b5855fc0b2b6b31c787cce0c61e528bb6
NicolaAstra <mcsnow78@gmail.com>
2019-10-01 21:00:45 +0700

    setup login form

--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MyButton from "../utils/button";
+import Login from "./login";
 
 const RegisterLogin = () => {
   return (
@@ -27,7 +28,7 @@ const RegisterLogin = () => {
           <div className="right">
             <h2>Registered Customers</h2>
             <p>If you have an account please log in.</p>
-            LOGIN
+            <Login />
           </div>
         </div>
       </div>
