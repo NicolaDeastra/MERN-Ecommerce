e6536ee5d4356e48325802c682f7b1e7bfc43e59
NicolaAstra <mcsnow78@gmail.com>
2019-10-05 19:48:59 +0700

    setup register commponent

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,11 +4,13 @@ import { Switch, Route } from "react-router-dom";
 import Layout from "./hoc/layout";
 import Home from "./components/Home";
 import RegisterLogin from "./components/Register_login";
+import Register from "./components/Register_login/register";
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/register" exact component={Register} />
         <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/" exact component={Home} />
       </Switch>
