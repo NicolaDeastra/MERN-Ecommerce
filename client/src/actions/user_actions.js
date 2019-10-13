94d7d331cdfabbe1c812d134acca3a739749bc8e
NicolaAstra <mcsnow78@gmail.com>
2019-10-13 17:46:43 +0700

    finish header links

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from "./types";
 
 import { USER_SERVER } from "../components/utils/misc";
 
@@ -32,6 +32,17 @@ export function auth() {
 
   return {
     type: AUTH_USER,
+    payload: request
+  };
+}
+
+export function logoutUser() {
+  const request = axios
+    .get(`${USER_SERVER}/logout`)
+    .then(response => response.data);
+
+  return {
+    type: LOGOUT_USER,
     payload: request
   };
 }
