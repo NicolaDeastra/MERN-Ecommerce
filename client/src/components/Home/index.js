aceedf84ef5eca4e849343c0e24a66580beddbd0
NicolaAstra <mcsnow78@gmail.com>
2019-10-14 20:49:58 +0700

    setup home slider and promotion

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import HomeSlider from "./home_slider";
+import HomePromotion from "./home_promotion";
 
 class Home extends Component {
   render() {
-    return <div>home</div>;
+    return (
+      <div>
+        <HomeSlider />
+        <HomePromotion />
+      </div>
+    );
   }
 }
 
