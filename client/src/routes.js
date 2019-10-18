db5ba4e72208546fb7d2b8d184b8f973848eef1d
NicolaAstra <mcsnow78@gmail.com>
2019-10-18 20:39:40 +0700

    setup shop component

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,6 +7,7 @@ import Auth from "./hoc/auth";
 import Home from "./components/Home";
 import RegisterLogin from "./components/Register_login";
 import Register from "./components/Register_login/register";
+import Shop from "./components/Shop";
 
 import UserDashboard from "./components/User";
 
@@ -25,6 +26,7 @@ const Routes = () => {
           exact
           component={Auth(RegisterLogin, false)}
         />
+        <Route path="/shop" exact component={Auth(Shop, null)} />
         <Route path="/" exact component={Auth(Home, null)} />
       </Switch>
     </Layout>
