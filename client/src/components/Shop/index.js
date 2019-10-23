610d41bcc60739d111629db132d312a2f1e97d89
NicolaAstra <mcsnow78@gmail.com>
2019-10-23 20:31:47 +0700

    finish radiocheckbox component

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import PageTop from "../utils/page_top";
 
-import { frets } from "../utils/Form/fixed_categories";
+import { frets, price } from "../utils/Form/fixed_categories";
 
 import { connect } from "react-redux";
 import { getBrands, getWoods } from "../../actions/products_actions";
 
 import CollapseCheckbox from "../utils/collapseCheckbox";
+import CollapseRadio from "../utils/collapseRadio";
 
 class Shop extends Component {
   state = {
@@ -26,9 +27,26 @@ class Shop extends Component {
     this.props.dispatch(getWoods());
   }
 
+  handlePrice = value => {
+    const data = price;
+    let array = [];
+
+    for (let key in data) {
+      if (data[key]._id === parseInt(value, 10)) {
+        array = data[key].array;
+      }
+    }
+    return array;
+  };
+
   handleFilters = (filters, category) => {
     const newFilters = { ...this.state.filters };
     newFilters[category] = filters;
+
+    if (category === "price") {
+      let priceValues = this.handlePrice(filters);
+      newFilters[category] = priceValues;
+    }
 
     this.setState({
       filters: newFilters
@@ -56,10 +74,16 @@ class Shop extends Component {
                 handleFilters={filters => this.handleFilters(filters, "frets")}
               />
               <CollapseCheckbox
-                initState={true}
+                initState={false}
                 title="Wood"
                 list={products.woods}
                 handleFilters={filters => this.handleFilters(filters, "wood")}
+              />
+              <CollapseRadio
+                initState={true}
+                title="Price"
+                list={price}
+                handleFilters={filters => this.handleFilters(filters, "price")}
               />
             </div>
             <div className="right">right</div>
