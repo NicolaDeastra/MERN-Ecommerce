c6aec8c8d1d99f8e59d8bae80db8f7c493f1d4b9
NicolaAstra <mcsnow78@gmail.com>
2019-11-01 22:57:35 +0700

    finish loadmore component

--- client/src/components/Shop/loadmoreCards.js
+++ client/src/components/Shop/loadmoreCards.js
@@ -7,6 +7,11 @@ const LoadmoreCards = props => {
       <div>
         <CardBlockShop grid={props.grid} list={props.products} />
       </div>
+      {props.size > 0 && props.size >= props.limit ? (
+        <div className="load_more_container">
+          <span onClick={() => props.loadMore()}>Load More</span>
+        </div>
+      ) : null}
     </div>
   );
 };
