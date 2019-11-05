6358c6532d7a3375b65c777dca299c2b3f80382d
NicolaAstra <mcsnow78@gmail.com>
2019-11-05 21:21:18 +0700

    setup add product form

--- client/src/components/utils/Form/formfield.js
+++ client/src/components/utils/Form/formfield.js
@@ -20,6 +20,9 @@ const Formfield = ({ formdata, change, id }) => {
       case "input":
         formTemplate = (
           <div className="formBlock">
+            {formdata.showlabel ? (
+              <div className="label_inputs">{formdata.config.label}</div>
+            ) : null}
             <input
               {...formdata.config}
               value={formdata.value}
