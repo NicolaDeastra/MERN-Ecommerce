1d7af45561dfafbbe268c82ee216b4cfb8e57190
NicolaAstra <mcsnow78@gmail.com>
2019-11-13 16:17:40 +0700

    setup manage categories

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -3,6 +3,7 @@ import {
   GET_PRODUCTS_BY_SELL,
   GET_PRODUCTS_BY_ARRIVAL,
   GET_BRANDS,
+  ADD_BRAND,
   GET_WOODS,
   GET_PRODUCTS_TO_SHOP,
   ADD_PRODUCT,
@@ -89,6 +90,23 @@ export function getBrands() {
 
   return {
     type: GET_BRANDS,
+    payload: request
+  };
+}
+
+export function addBrand(datatoSubmit, existingBrands) {
+  const request = axios
+    .post(`${PRODUCT_SERVER}/brand`, datatoSubmit)
+    .then(response => {
+      let brands = [...existingBrands, response.data.brand];
+      return {
+        sucess: response.data.sucess,
+        brands
+      };
+    });
+
+  return {
+    type: ADD_BRAND,
     payload: request
   };
 }
--- client/src/reducers/products_reducer.js
+++ client/src/reducers/products_reducer.js
@@ -2,6 +2,7 @@ import {
   GET_PRODUCTS_BY_SELL,
   GET_PRODUCTS_BY_ARRIVAL,
   GET_BRANDS,
+  ADD_BRAND,
   GET_WOODS,
   GET_PRODUCTS_TO_SHOP,
   ADD_PRODUCT,
@@ -16,6 +17,12 @@ export default function(state = {}, action) {
       return { ...state, byArrival: action.payload };
     case GET_BRANDS:
       return { ...state, brands: action.payload };
+    case ADD_BRAND:
+      return {
+        ...state,
+        addBrand: action.payload.success,
+        brands: action.payload.brands
+      };
     case GET_WOODS:
       return { ...state, woods: action.payload };
     case GET_PRODUCTS_TO_SHOP:
