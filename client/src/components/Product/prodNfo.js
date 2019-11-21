e18ea8c94afde6715e078e4a9b92a1412a78c5c4
NicolaAstra <mcsnow78@gmail.com>
2019-11-21 21:55:02 +0700

    add content to detail product

--- client/src/components/Product/prodNfo.js
+++ client/src/components/Product/prodNfo.js
@@ -7,6 +7,71 @@ import faCheck from "@fortawesome/fontawesome-free-solid/faCheck";
 import faTimes from "@fortawesome/fontawesome-free-solid/faTimes";
 
 const ProdNfo = props => {
+  const showProdTags = detail => (
+    <div className="product_tags">
+      {detail.shipping ? (
+        <div className="tag">
+          <div>
+            <FontAwesomeIcon icon={faTruck} />
+          </div>
+          <div className="tag_text">
+            <div>Free shipping</div>
+            <div>And return</div>
+          </div>
+        </div>
+      ) : null}
+      {detail.available ? (
+        <div className="tag">
+          <div>
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <div className="tag_text">
+            <div>Available</div>
+            <div>in store</div>
+          </div>
+        </div>
+      ) : (
+        <div className="tag">
+          <div>
+            <FontAwesomeIcon icon={faTimes} />
+          </div>
+          <div className="tag_text">
+            <div>Not Available</div>
+            <div>Preorder only</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+
+  const showProdActions = detail => (
+    <div className="product_actions">
+      <div className="price">Rp.{detail.price}</div>
+      <div className="cart">
+        <MyButton
+          type="add_to_cart_link"
+          runAction={() => {
+            console.log("add to cart");
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  const showProdSpecifications = detail => (
+    <div className="product_specifications">
+      <h2>SPecs:</h2>
+      <div>
+        <div className="item">
+          <strong>Frets:</strong> {detail.frets}
+        </div>
+        <div className="item">
+          <strong>Wood:</strong> {detail.wood.name}
+        </div>
+      </div>
+    </div>
+  );
+
   const detail = props.detail;
   return (
     <div>
@@ -15,6 +80,9 @@ const ProdNfo = props => {
         {detail.name}
       </h1>
       <p>{detail.description}</p>
+      {showProdTags(detail)}
+      {showProdActions(detail)}
+      {showProdSpecifications(detail)}
     </div>
   );
 };
